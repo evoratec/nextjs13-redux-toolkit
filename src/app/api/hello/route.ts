c1042f54e5c82b3a631f0859939ae543cf1cf213
Juan Antonio Navarro <evoratec@gmail.com>
2023-04-09 12:38:17 +0200

    Cambio en la ruta hello

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,3 +1,16 @@
+import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
-  return new Response('Hello, Next.js!')
+  let parametros = {puesto_id:"ibericos-el-senorito"};
+  const res = await fetch('https://api.mercadodenumancia.es/api/productos/lista', {
+    method: "POST",
+    body: JSON.stringify(parametros),
+
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+  });
+  const data = await res.json();
+
+  return NextResponse.json({ data })
 }
