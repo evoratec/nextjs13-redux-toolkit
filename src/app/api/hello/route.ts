8c4bd0c05e6ca7e85ffa2e0bf6e582475089661e
Juan Antonio Navarro <syntonize@syntonize.com>
2023-08-26 21:45:50 +0200

    cambios

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 export async function GET(request: Request) {
   let parametros = {puesto_id:"ibericos-el-senorito"};
-  const res = await fetch('https://api.mercadodenumancia.es/api/productos/lista', {
+  const res = await fetch('https://api.mercadodenumancia.es/api/productos/lista-categorias', {
     method: "POST",
     body: JSON.stringify(parametros),
 
